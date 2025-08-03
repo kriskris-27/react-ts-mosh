32d4d27586370a9fb98172dedb18c6863eb14c67
Kris <krishnakumars0101@gmail.com>
2025-08-03 19:53:18 +0530

    usememo example

--- src/useMemo/SimExMemo.tsx
+++ src/useMemo/SimExMemo.tsx
@@ -1,19 +1,36 @@
-import React, { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 const SimExMemo = () => {
     const [number,setNumber] = useState<number>(0)
     const [theme,setTheme] = useState<boolean>(true)
     
-    const double = (()=>{
+    // const double = (()=>{
         
+    //     console.log("Calculating...");
+    //     return number * 2
+    // })()
+
+    const double = useMemo(()=>{
         console.log("Calculating...");
-        return number * 2
-    })()
+        return number * 2;
+        
+    },[number])
+
+    const themeStyle = {
+    backgroundColor: theme ? '#fff' : '#333',
+    color: theme ? '#000' : '#fff',
+    padding: '10px',
+    marginTop: '20px',
+  };
    return (
-    <>
+    < >
+    <div style={themeStyle}>
+
+
     <input type='number' value={number} onChange={(e)=>setNumber(+e.target.value)}/>
     <button onClick={()=>setTheme(prev=>!prev)}>click to switch theme</button>
     <p>double {double}</p>
+        </div>
     </>
   )
 }
